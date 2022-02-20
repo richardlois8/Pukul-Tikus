85f76473947b9c269f2f8a9ef9744b264cb3e3f9
richardlois8 <78334592+richardlois8@users.noreply.github.com>
2022-02-20 14:00:23 +0700

    Menambahkan Program Pukul Tikus 1.0

---
+++ Pukul TIkus/Pukul TIkus/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblSkor = New System.Windows.Forms.Label()
+        Me.lblRandom = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.TimerTikus = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'lblSkor
+        '
+        Me.lblSkor.AutoSize = True
+        Me.lblSkor.Location = New System.Drawing.Point(20, 296)
+        Me.lblSkor.Name = "lblSkor"
+        Me.lblSkor.Size = New System.Drawing.Size(45, 15)
+        Me.lblSkor.TabIndex = 0
+        Me.lblSkor.Text = "Skor : 0"
+        '
+        'lblRandom
+        '
+        Me.lblRandom.AutoSize = True
+        Me.lblRandom.Location = New System.Drawing.Point(302, 296)
+        Me.lblRandom.Name = "lblRandom"
+        Me.lblRandom.Size = New System.Drawing.Size(58, 15)
+        Me.lblRandom.TabIndex = 1
+        Me.lblRandom.Text = "Random: "
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(162, 292)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 2
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'TimerTikus
+        '
+        Me.TimerTikus.Interval = 800
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(372, 274)
+        Me.Panel1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 323)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblRandom)
+        Me.Controls.Add(Me.lblSkor)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Pukul Tikus"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblSkor As Label
+    Friend WithEvents lblRandom As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents TimerTikus As Timer
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Pukul TIkus/Pukul TIkus/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+
+    Dim rnd As Random = New Random()
+    Dim arrayPosKolom() As Integer = New Integer() {8, 79, 150, 221, 292}
+    Dim arrayPosBaris() As Integer = New Integer() {6, 70, 131, 195}
+    Dim btn As New Button()
+    Dim skor As Integer
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If btnStart.Text = "Start" Then
+            btnStart.Text = "Stop"
+            TimerTikus.Enabled = True
+        Else
+            btnStart.Text = "Start"
+            TimerTikus.Enabled = False
+            MsgBox(lblSkor.Text,, "Hasil Game")
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub TimerTikus_Tick(sender As Object, e As EventArgs) Handles TimerTikus.Tick
+        Dim posKolom As Integer = arrayPosKolom(rnd.Next(0, arrayPosKolom.Count))
+        Dim posBaris As Integer = arrayPosBaris(rnd.Next(0, arrayPosBaris.Count))
+
+        btn = New Button()
+        btn.Name = "btnTikus"
+        btn.Location = New Point(posKolom, posBaris)
+        btn.Size = New Size(65, 58)
+        btn.Image = Image.FromFile("tikus2.png")
+        btn.BringToFront()
+        lblRandom.Text = "Random: " & CStr(posKolom) & "," & CStr(posBaris)
+        Panel1.Controls.Add(btn)
+        AddHandler btn.Click, AddressOf Me.buttonClick
+    End Sub
+
+    Private Sub buttonClick(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim btnClick As Button
+        btnClick = DirectCast(sender, Button)
+        skor += 100
+        lblSkor.Text = "Skor : " & skor
+        Panel1.Controls.Remove(btnClick)
+    End Sub
+End Class
